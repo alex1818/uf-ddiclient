d54d40419c61b67c9a95499665b465c645b45b1a
Daniele Sergio <daniele.sergio@kynetics.com>
2018-11-18 12:18:55 +0100

    Implement ApplyingSoftwareModuleMiddleware

---
+++ ddiclient-core/src/main/kotlin/com/kynetics/updatefactory/ddiclient/corek/model/middleware/ApplyingSoftwareModuleMiddleware.kt
@@ -0,0 +1,47 @@
+/*
+ *   Copyright © 2017-2018 Kynetics LLC
+ *
+ *   All rights reserved. This program and the accomp*ing materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   which accompanies this distribution, and is available at
+ *   http://www.eclipse.org/legal/epl-v10.htm
+ */
+
+package com.kynetics.updatefactory.ddiclient.corek.model.middleware
+
+import com.kynetics.updatefactory.ddiclient.core.servicecallback.SystemOperation
+import com.kynetics.updatefactory.ddiclient.corek.model.Error
+import com.kynetics.updatefactory.ddiclient.corek.model.EventPublisher
+import com.kynetics.updatefactory.ddiclient.corek.model.UFEvent
+import com.kynetics.updatefactory.ddiclient.corek.model.UFState
+
+/**
+ * @author Daniele Sergio
+ */
+class ApplyingSoftwareModuleMiddleware(private val eventPublisher: EventPublisher,
+                                       private val systemOperation: SystemOperation): AbstractUFMiddleware(
+        Pair(UFState.Name.WAITING_UPDATE_AUTHORIZATION, UFEvent.Name.AUTHORIZATION_GRANTED),
+        Pair(UFState.Name.SAVING_FILE, UFEvent.Name.FILE_SAVED)) {
+
+    override fun execute(state: UFState, action: UFEvent<*>): UFEvent<*> {
+        if(state.name == UFState.Name.WAITING_UPDATE_AUTHORIZATION || state.data.isUpdateForced!!){
+            this.executeUpdate(state)
+        }
+        return action
+    }
+
+    private fun executeUpdate(state: UFState) {
+        Thread {
+            //todo replace with coroutine
+            systemOperation.executeUpdate(state.data.actionId!!)
+            when (systemOperation.updateStatus()!!) {
+                SystemOperation.UpdateStatus.APPLIED_WITH_ERROR ->
+                    eventPublisher.publishEvent(event = UFEvent.newUpdateSuccessEvent)
+                SystemOperation.UpdateStatus.SUCCESSFULLY_APPLIED ->
+                    eventPublisher.publishEvent(event = UFEvent.newUpdateErrorEvent(Error(details = arrayOf("Error"))))
+                SystemOperation.UpdateStatus.NOT_APPLIED -> {
+                }
+            }
+        }.start()
+    }
+}
